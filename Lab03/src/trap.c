5265da4137773ee7e83530ca9a653a8e8f635c50
Gabriel <gabrielgchung@gmail.com>
2024-04-23 20:03:29 -0300

    Reorganizado em pastas e prints e documentos de execuçao adicionado

--- Lab03/src/trap.c
+++ Lab03/src/trap.c
@@ -25,32 +25,25 @@ void trapezoid(double point_a, double point_b, int trapezoid_number, double* res
         local_x = local_a + i * trapezoid_height;
         local_result += sin(local_x);
     }
-	
-    
+
     local_result *= trapezoid_height;
-    #pragma omp critical 
-    {
-   	 double aux = *result_ptr; //testar condicao de corrida
     
-   	 for (int i = 0; i < 10000; i++) *result_ptr = i; //testar condicao de corrida
-    
-   	 *result_ptr = aux + local_result;
-    }
+    *result_ptr += local_result;
 }
 
 int main(int argc, char* argv[])
 {
-    int thread_count = 1; // numero de threads
-    int n = 2400000;        // número de trapézios
+    int thread_count = 1;   // numero de threads
+    int n = 24000000;        // número de trapézios
     double result = 0; // resultado da aproximação
     double a = 0;      // limite inferior da integral
     double b = 100;    // limite superior da integral
-	
+
     if (argc > 1) // caso queira colocar um valor para número de threads
     {
         thread_count = strtol(argv[1], NULL, 10);
     }
-
+	
     #pragma omp parallel num_threads(thread_count)
     trapezoid(a, b, n, &result);
 
@@ -59,4 +52,5 @@ int main(int argc, char* argv[])
     printf("a = %.2f\n", a);
     printf("b = %.2f\n", b);
     printf("\nresultado aproximado: %.10lf...\n", result);
+   
 }
