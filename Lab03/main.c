d02c222db5ee1695e0053e2867fdb93c6da35d49
gabrielc231 <142500547+gabrielc231@users.noreply.github.com>
2024-04-23 16:23:50 -0300

    código do método dos trapézios atualizado

--- Lab03/main.c
+++ Lab03/main.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <omp.h>
 
-double a = 0;   // ponto a
-double b = 100; // ponto b
-int n = 8;      // número de trapézios
-
-int main()
+void trapezoid(double point_a, double point_b, long trapezoid_number, double* global_result_p)
 {
-    double h = (b - a) / n;
-    double approx = (sin(a) + sin(b)) / 2.0;
+    double trapezoid_height;
+    double local_x; // entrada
+    double local_a;
+    double local_b;
+    long local_trapezoid_number;
+    int local_result;
+    int my_rank = omp_get_thread_num();
+    int local_thread_count = omp_get_num_threads();
 
-#pragma omp parallel
+    trapezoid_height = (point_b - point_a) / trapezoid_number;
+    local_trapezoid_number = trapezoid_number / local_thread_count;
+    local_a = point_a + my_rank * trapezoid_number * trapezoid_height;
+    local_b = local_a + local_trapezoid_number * trapezoid_height;
+    local_result = (sin(local_a) + sin(local_b)) / 2.0;
+
+    for (int i = 0; i <= local_trapezoid_number - 1; i++)
     {
-        for (int i = 0; i < n; i++)
-        {
-            int x_i = a + i * h;
-            approx += sin(x_i);
-        }
+        local_x = local_a + i * trapezoid_height;
+        local_result += sin(local_x);
     }
 
-    approx = h * approx;
+    local_result *= trapezoid_height;
+}
+
+int main(int argc, char* argv[])
+{
+    int thread_count = 1;
+    long n = 8;        // número de trapézios
+    double result = 0; // resultado da aproximação
+    double a = 0;      // limite inferior da integral
+    double b = 100;    // limite superior da integral
+
+    if (argc > 1) // caso queira colocar um valor para número de threads
+    {
+        thread_count = strtol(argv[1], NULL, 10);
+    }
+
+#pragma omp parallel num_threads(thread_count)
+    trapezoid(a, b, n, &result);
+
+    printf("thread_count = %d\n", thread_count);
+    printf("n = %ld\n", n);
+    printf("a = %.2f\n", a);
+    printf("b = %.2f\n", b);
+    printf("\n resultado aproximado: %lf...", result);
 }
